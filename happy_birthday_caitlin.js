fcca3a623a14b0bb8828e2e552953ea28ae7b710
DJ Carroll <dcarroll242@gmail.com>
2019-09-25 22:04:05 -0500

    bday updates

--- happy_birthday_caitlin.js
+++ happy_birthday_caitlin.js
@@ -36,6 +36,7 @@ function setup() {
                    Math.random()*100,
                    loadImage(balloonImages[Math.floor(Math.random()*balloonImages.length)])]);
   }
+  balloons.sort(function(a, b){return a[2]-b[2];})
 }
 
 function draw() {
@@ -60,20 +61,22 @@ function drawKey(){
   textSize(30);
   textAlign(LEFT);
   textStyle(NORMAL);
-  fill(100, 0, 0);
+  textFont("Oswald");
+  fill(50, 50, 50);
   text("Press [k] to show/hide this key.", 10, 30);
   textSize(20);
   text("Happy Birthday: Click Anywhere (except Luciana)", 10, 60);
-  text("Luciana Message: Click Luciana", 10, 90);
-  text("Stop Current Sound: Spacebar", 10, 120);
+  text("Luciana Message: Click on Luciana", 10, 90);
+  text("Stop Current Sound: Press Spacebar", 10, 120);
 }
 
 function drawSmallKey(){
   textSize(14);
   textAlign(LEFT);
-  textStyle(ITALIC);
+  //textStyle(ITALIC);
+  textFont("Oswald");
   fill(100, 100, 100);
-  text("Show Key [k]", 4, 14);
+  text("Show Key [k]", 2, 14);
 }
 
 function drawBalloons() {
@@ -93,27 +96,34 @@ function drawHappyBirthday() {
   textSize(102);
   textAlign(CENTER);
   textStyle(NORMAL);
+  textFont("Mansalva");
   fill(0, 0, 0);
   text("Happy Birthday", mouseX, mouseY - 40);
   text("Caitlin!!!", mouseX, mouseY + 100);
 }
 
 function drawSignature(){
+  let anchorX = -700;
+  let anchorY = -100;
   textSize(40);
   textAlign(LEFT);
   textStyle(NORMAL);
+  textFont("Mansalva");
   fill(0, 0, 0);
-  text("Love,", windowWidth/2-200, windowHeight - 110);
-  text("DJ, Ingrid, Luciana, & Grandpa", windowWidth/2-200+50, windowHeight - 50);
+  text("Love,", windowWidth + anchorX, windowHeight + anchorY);
+  text("DJ, Ingrid, Luciana, & Grandpa", windowWidth + anchorX + 50, windowHeight + anchorY + 60);
 }
 
 function drawMessage(){
+  let anchorX = -700;
+  let anchorY = -250;
   textSize(40);
   textAlign(LEFT);
   textStyle(NORMAL);
+  textFont("Mansalva");
   fill(0, 0, 0);
-  text("Have a wonderful birthday Caitlin!", windowWidth/2-220, windowHeight/2 + 100);
-  text("You deserve it & we love you so much!", windowWidth/2-220, windowHeight/2 + 150);
+  text("Have a wonderful birthday Caitlin!", windowWidth + anchorX, windowHeight + anchorY);
+  text("You deserve it & we love you so much!", windowWidth + anchorX, windowHeight + anchorY + 60);
 }
 
 function songUploaded(song){
@@ -150,5 +160,6 @@ function windowResized() {
     balloons[i][1] = Math.random()*windowHeight;
     balloons[i][2] = Math.random()*100;
   }
+  balloons.sort(function(a, b){return a[2]-b[2];})
   pause = false;
 }
