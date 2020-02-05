c0c69094991162bcfa66fcf3e211a337744a705d
DJ Carroll <dcarroll242@gmail.com>
2020-02-04 20:54:49 -0600

    test changes

--- rotation_test.js
+++ rotation_test.js
@@ -1,18 +1,51 @@
+let alpha = 0, beta = 0, gamma = 0;
+
+
+function preload() {
+  inconsolata = loadFont('assets/fonts/inconsolata.woff');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+
+  window.addEventListener("deviceorientation", function(event) {
+    alpha = event.alpha;
+    beta = event.beta;
+    gamma = event.gamma;
+  }, true);
+
+  textAlign(CENTER, CENTER);
+  textFont(inconsolata);
+  textSize(100);
+
 }
 
 function draw() {
-  background(0);
+  background(100);
+
+  text("(" + alpha + "," + beta + "," + gamma + ")", 0, -250, 0);
+
+  push();
+  translate(-300, 0, 0);
+  rotateY(radians(360*mouseY/height));
+  box(100, 100, 100);
+  pop();
+
+  push();
+  translate(300, 0, 0);
+  rotateY(radians(-360*mouseY/height));
+  box(100, 100, 100);
+  pop();
+
+  push();
   rotateZ(radians(rotationZ));
   rotateX(radians(rotationX));
   rotateY(radians(rotationY));
-
   rotateZ(radians(360*mouseX/width));
   rotateX(radians(360*mouseY/width));
   rotateY(radians(360*mouseY/width));
-
   box(200, 200, 200);
+  pop();
 }
 
 function windowResized() {
